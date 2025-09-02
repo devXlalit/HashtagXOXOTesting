271e50c2cb60ceb31e83ff95f157454e6ec5e824
devXlalit <lalitpagare.dev@gmail.com>
2025-09-02 20:55:24 +0530

    payment-issue-fix-v7

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 connectDB();
 connectCloudinary();
 
